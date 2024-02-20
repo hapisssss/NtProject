73ee6135e9a4caa1c64b16743e63a8e7037501d1
Hafizh Satria Buana <hafizsatiria@student.ub.ac.id>
2024-02-20 10:06:45 +0700

    notif-expo-try

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
+import { useState, useEffect, useRef } from 'react';
 import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
+import { firebase } from '@react-native-firebase/messaging';
+
+
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -8,6 +13,32 @@ Notifications.setNotificationHandler({
     shouldSetBadge: false,
   }),
 });
+export default function App() {
+  const [expoPushToken] = useState('');
+  const [notification] = useState(false);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
+  );
+}
 async function schedulePushNotification() {
   await Notifications.scheduleNotificationAsync({
     content: {
@@ -18,26 +49,5 @@ async function schedulePushNotification() {
     trigger: { seconds: 1 },
   });
 }
-
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}>
-      <Button
-        title="Press to schedule a notification"
-        onPress={async () => {
-          await schedulePushNotification();
-        }}
-      />
-    </View>
-  );
-}
-
-
-
 
 
